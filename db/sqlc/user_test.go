27b10e50169a8c67dd099a427fad2c61c7213fe9
alex-davis-ang <charles.leonard.com@gmail.com>
2024-10-10 20:08:36 +0900

    user test added

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/charles-leonard/bank_system/util"
 	"github.com/stretchr/testify/require"
@@ -44,4 +45,7 @@ func TestGetUser(t *testing.T) {
 	require.Equal(t, user1.HashedPassword, user2.HashedPassword)
 	require.Equal(t, user1.FullName, user2.FullName)
 	require.Equal(t, user1.Email, user2.Email)
+	require.WithinDuration(t, user1.PasswordChangedAt, user2.PasswordChangedAt, time.Second)
+	require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+
 }
