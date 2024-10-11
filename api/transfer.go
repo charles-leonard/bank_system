abed999a1a4b541a45a13817050aa8d79db14180
alex-davis-ang <charles.leonard.com@gmail.com>
2024-10-12 05:11:12 +0900

    test mi 1

--- api/transfer.go
+++ api/transfer.go
@@ -2,10 +2,12 @@ package api
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"net/http"
 
 	db "github.com/charles-leonard/bank_system/db/sqlc"
+	"github.com/charles-leonard/bank_system/token"
 	"github.com/gin-gonic/gin"
 )
 
@@ -23,11 +25,21 @@ func (server *Server) createTransfer(ctx *gin.Context) {
 		return
 	}
 
-	if !server.validaAccount(ctx, req.FromAccountID, req.Currency) {
+	fromAccount, valid := server.validaAccount(ctx, req.FromAccountID, req.Currency)
+	if !valid {
 		return
 	}
 
-	if !server.validaAccount(ctx, req.FromAccountID, req.Currency) {
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
+	if fromAccount.Owner != authPayload.Username {
+		err := errors.New("from account doesnt belong to the authenticated user")
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+
+	_, valid = server.validaAccount(ctx, req.FromAccountID, req.Currency)
+
+	if !valid {
 		return
 	}
 
@@ -47,21 +59,21 @@ func (server *Server) createTransfer(ctx *gin.Context) {
 
 }
 
-func (server *Server) validaAccount(ctx *gin.Context, accountID int64, currency string) bool {
+func (server *Server) validaAccount(ctx *gin.Context, accountID int64, currency string) (db.Account, bool) {
 	account, err := server.store.GetAccount(ctx, accountID)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			ctx.JSON(http.StatusNotFound, errorResponse((err)))
-			return false
+			return account, false
 		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-		return false
+		return account, false
 	}
 	if account.Currency != currency {
 		err := fmt.Errorf("account {%d} currency mismatch: %s vs %s", account.ID, account.Currency, currency)
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-		return false
+		return account, false
 	}
 
-	return true
+	return account, true
 }
