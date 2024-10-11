22b87c23dc5ca07c8c477d2846ead1823b077798
alex-davis-ang <charles.leonard.com@gmail.com>
2024-10-11 20:27:57 +0900

    token structure changed

--- api/account_test.go
+++ api/account_test.go
@@ -92,7 +92,7 @@ func TestGetAccountAPI(t *testing.T) {
 
 			store := mockdb.NewMockStore(ctrl)
 			tc.buildStubs(store)
-			Server := NewServer(store)
+			Server := newTestServer(t, store)
 			recorder := httptest.NewRecorder()
 
 			url := fmt.Sprintf("/accounts/%d", tc.accountID)
